0dd149486d8f6184ca1f51489cbe91c74844a9f6
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-10-28 16:05:02 +0000

    Adding collision detection, adding win/lose acknowledgement.

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ import {
 } from './setup'
 // Helpers
 import { createBricks } from "./helpers";
+import { Collision } from "./Collision";
 
 let gameOver = false;
 let score = 0;
@@ -39,6 +40,7 @@ function gameLoop(
   bricks: Brick[],
   paddle: Paddle,
   ball: Ball,
+  collision: Collision,
 ) {
   view.clear();
   view.drawBricks(bricks)
@@ -55,7 +57,22 @@ function gameLoop(
     paddle.movePaddle()
   }
 
-  requestAnimationFrame(() => gameLoop(view, bricks, paddle, ball))
+  collision.checkBallCollision(ball, paddle, view)
+  const collidingBrick = collision.isCollidingBricks(ball, bricks);
+
+  if (collidingBrick) {
+    score += 1;
+    view.drawScore(score);
+  }
+
+  // Game over when ball leave play field 
+  if (ball.pos.y > view.canvas.height) gameOver = true;
+  // if game won, set game over and display win 
+  if (bricks.length === 0) return setGameWin(view)
+  // Return if game over and don't run the requestAnimationFrame 
+  if (gameOver) return setGameOver(view)
+
+  requestAnimationFrame(() => gameLoop(view, bricks, paddle, ball, collision))
 }
 
 
@@ -64,6 +81,8 @@ function startGame(view: CanvasView) {
   score = 0;
   view.drawInfo('')
   view.drawScore(0)
+  //Create a collision instance 
+  const collision = new Collision();
   // Create all bricks 
   const bricks = createBricks()
   // Create a ball
@@ -86,7 +105,7 @@ function startGame(view: CanvasView) {
     PADDLE_IMAGE
   )
 
-  gameLoop(view, bricks, paddle, ball)
+  gameLoop(view, bricks, paddle, ball, collision)
 }
 
 // Create a new view 
